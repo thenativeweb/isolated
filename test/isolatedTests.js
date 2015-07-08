95444e224bc5b2b2fa4e76394f506b4ff33a978e
Golo Roden <webmaster@goloroden.de>
2015-07-08 21:49:40 +0200

    Updated dependencies.

--- test/isolatedTests.js
+++ test/isolatedTests.js
@@ -3,7 +3,7 @@
 var fs = require('fs'),
     path = require('path');
 
-var assert = require('node-assertthat');
+var assert = require('assertthat');
 
 var isolated = require('../lib/isolated');
 
@@ -13,49 +13,57 @@ var bar = path.join(__dirname, 'data', 'bar.txt'),
 var data = path.join(__dirname, 'data');
 
 suite('isolated', function () {
-  /*eslint-disable handle-callback-err*/
+  /* eslint-disable handle-callback-err */
   test('returns an empty directory.', function (done) {
-    isolated(function (err, directory) {
+    isolated(function (errIsolated, directory) {
+      assert.that(errIsolated).is.null();
+
       fs.readdir(directory, function (err, files) {
-        assert.that(err, is.null());
-        assert.that(files.length, is.equalTo(0));
+        assert.that(err).is.null();
+        assert.that(files.length).is.equalTo(0);
         done();
       });
     });
   });
 
   test('copies the specified file to the isolated directory.', function (done) {
-    isolated(foo, function (err, directory) {
+    isolated(foo, function (errIsolated, directory) {
+      assert.that(errIsolated).is.null();
+
       fs.readdir(directory, function (err, files) {
-        assert.that(err, is.null());
-        assert.that(files.length, is.equalTo(1));
-        assert.that(files[0], is.equalTo('foo.txt'));
+        assert.that(err).is.null();
+        assert.that(files.length).is.equalTo(1);
+        assert.that(files[0]).is.equalTo('foo.txt');
         done();
       });
     });
   });
 
   test('copies the specified files to the isolated directory.', function (done) {
-    isolated([ foo, bar ], function (err, directory) {
+    isolated([ foo, bar ], function (errIsolated, directory) {
+      assert.that(errIsolated).is.null();
+
       fs.readdir(directory, function (err, files) {
-        assert.that(err, is.null());
-        assert.that(files.length, is.equalTo(2));
-        assert.that(files.indexOf('foo.txt'), is.not.equalTo(-1));
-        assert.that(files.indexOf('bar.txt'), is.not.equalTo(-1));
+        assert.that(err).is.null();
+        assert.that(files.length).is.equalTo(2);
+        assert.that(files.indexOf('foo.txt')).is.not.equalTo(-1);
+        assert.that(files.indexOf('bar.txt')).is.not.equalTo(-1);
         done();
       });
     });
   });
 
   test('copies the specified directory to the isolated directory.', function (done) {
-    isolated(data, function (err, directory) {
+    isolated(data, function (errIsolated, directory) {
+      assert.that(errIsolated).is.null();
+
       fs.readdir(directory, function (err, files) {
-        assert.that(err, is.null());
-        assert.that(files.length, is.equalTo(1));
-        assert.that(files[0], is.equalTo('data'));
+        assert.that(err).is.null();
+        assert.that(files.length).is.equalTo(1);
+        assert.that(files[0]).is.equalTo('data');
         done();
       });
     });
   });
-  /*eslint-enable handle-callback-err*/
+  /* eslint-enable handle-callback-err */
 });
